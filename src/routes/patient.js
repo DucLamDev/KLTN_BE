eb5bedbdad4769273da7bf3316ba636528d180e2
Bui Tran Thien An <buitranthienan1111@gmail.com>
2024-11-13 01:06:49 +0700

    create: patient service, controllers, routes

--- src/routes/patient.js
+++ src/routes/patient.js
@@ -1,70 +1,25 @@
 import express from "express";
+import {
+  createPatientController,
+  getListPatientsController,
+  getOnePatientByIdController,
+  updatePatientByIdController,
+  deletePatientByIdController,
+} from "../controllers/patientController.js";
 import Patient from "../models/Patient.js";
 
 const router = express.Router();
 
-// Tạo bệnh nhân mới
-router.post("/", async (req, res) => {
-  try {
-    const patient = new Patient(req.body);
-    await patient.save();
-    res.status(201).send(patient);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+router.post("/", createPatientController);
 
-// Lấy danh sách bệnh nhân (truyền vào email hoặc không)
-router.get("/", async (req, res) => {
-  try {
-    let patients;
-    const { email } = req.query;
-    if (!email) {
-      patients = await Patient.find();
-    } else patients = await Patient.findOne({ email });
-    res.status(200).send(patients);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-  
-// Lấy chi tiết một bệnh nhân
-router.get("/:id", async (req, res) => {
-  try {
-    const patient = await Patient.findById(req.params.id);
-    if (!patient) return res.status(404).send();
-    res.status(200).send(patient);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+router.get("/", getListPatientsController);
 
-// Cập nhật thông tin bệnh nhân
-router.put("/:id", async (req, res) => {
-  try {
-    const patient = await Patient.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!patient) return res.status(404).send();
-    res.status(200).send(patient);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+router.get("/:id", getOnePatientByIdController);
 
-// Xóa bệnh nhân
-router.delete("/:id", async (req, res) => {
-  try {
-    const patient = await Patient.findByIdAndDelete(req.params.id);
-    if (!patient) return res.status(404).send();
-    res.status(200).send(patient);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+router.put("/:id", updatePatientByIdController);
 
-// Lấy bệnh nhân theo accountId
+router.delete("/:id", deletePatientByIdController);
+
 router.get("/clerk/:clerkId", async (req, res) => {
   try {
     const { clerkId } = req.params;
